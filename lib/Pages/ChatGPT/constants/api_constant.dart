9d67e429c00287cdd88e9c4dcb638807a79fdaf2
WeiLun <lohweilun88@gmail.com>
2023-12-26 14:54:37 +0800

    ass

--- lib/Pages/ChatGPT/constants/api_constant.dart
+++ lib/Pages/ChatGPT/constants/api_constant.dart
@@ -1,2 +1,2 @@
 String BASE_URL = "https://api.openai.com/v1";
-String API_KEY = "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";
+String API_KEY = "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";
